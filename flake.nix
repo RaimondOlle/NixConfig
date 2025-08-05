4985c4f0b941b5d0f26d19cc46804f90f4c31063
Raimond Olle <raimond.olle@gmail.com>
2025-08-05 12:39:57 +0300

    changing shit

--- flake.nix
+++ flake.nix
@@ -8,21 +8,21 @@
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
-	  nixosConfigurations = {
-		  TestdrivingNixos-btw = nixpkgs.lib.nixosSystem {
-			  system = "x86_64-linux";
-			  modules = [ 
-				  ./configuration.nix
-				  home-manager.nixosModules.home-manager
-				  {
-					  home-manager.useGlobalPkgs = true;
-					  home-manager.useUserPackages = true;
-					  home-manager.users.biggusdickus = ./home.nix;
-					  home-manager.backupFileExtension = "backup";
-# Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
-				  }
-			  ];
-		  };
-	  };
+    nixosConfigurations = {
+      TestdrivingNixos-btw = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ 
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.biggusdickus = ./home.nix;
+            home-manager.backupFileExtension = "backup";
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
+      };
+    };
   };
 }
